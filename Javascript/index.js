d7e362caf01e44ad9041663afc54108bfd6a11ee
Bazi Mtsweni <bmtsweni4@gmail.com>
2022-02-22 16:25:28 +0200

    Added media queries for mobile on the home page

---
+++ Javascript/index.js
@@ -0,0 +1,7 @@
+const menu = document.getElementById("menu-btn");
+const nav = document.querySelector(".navigation");
+
+menu.addEventListener("click", ()=>{
+    // nav.classList.toggle("active");
+    console.log("Working");
+})
