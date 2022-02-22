1c038617f990dbeb49b54dfd64c1b6c794879579
Bazi Mtsweni <bmtsweni4@gmail.com>
2022-02-22 18:27:04 +0200

    fixed mobile menu

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,7 +1,36 @@
-const menu = document.getElementById("menu-btn");
-const nav = document.querySelector(".navigation");
+const menuBtn = document.querySelector(".nav-btn");
+const navList = document.querySelector(".navigation");
+const barsIcon = document.querySelector(" .fa-bars");
+const closeIcon = document.querySelector(" .fa-xmark");
 
-menu.addEventListener("click", ()=>{
-    // nav.classList.toggle("active");
-    console.log("Working");
+var active = false;
+
+menuBtn.addEventListener("click", ()=>{
+    if(!active){
+        navList.classList.add("active");
+        // btnIcon.classList.remove("fa-bars");
+        barsIcon.style.display = "none";
+        closeIcon.style.display = "block";
+
+        active = true;
+    }
+    else {
+
+        navList.classList.remove("active");
+
+        active = false
+    }
 })
+
+// menuBtn.addEventListener('click', () => {
+//     navList.classList.toggle('active');
+//     if (btnIcon.classList.contains('fa-bars')) {
+//         btnIcon.classList.remove('fa-bars');
+//         btnIcon.classList.add('fa-xmark');
+
+//     }
+//     else {
+//         btnIcon.classList.remove('fa-xmark');
+//         btnIcon.classList.add('fa-bars');
+//     }
+// });
