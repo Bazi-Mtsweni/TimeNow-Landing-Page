5f0bf4cb640462d974fdab1779da090670c64508
Bazi Mtsweni <bmtsweni4@gmail.com>
2022-02-24 03:29:18 +0200

    Fixed mobile menu icon

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,23 +1,24 @@
 const menuBtn = document.querySelector(".nav-btn");
 const navList = document.querySelector(".navigation");
-const barsIcon = document.querySelector(" .fa-bars");
-const closeIcon = document.querySelector(" .fa-xmark");
 
 var active = false;
 
 menuBtn.addEventListener("click", ()=>{
+
+    const barsIcon = document.querySelector(" .fa-bars");
+    const closeIcon = document.querySelector(" .fa-xmark");
+
     if(!active){
         navList.classList.add("active");
-        // btnIcon.classList.remove("fa-bars");
         barsIcon.style.display = "none";
         closeIcon.style.display = "block";
-
         active = true;
     }
     else {
 
         navList.classList.remove("active");
-
+        barsIcon.style.display = "block";
+        closeIcon.style.display = "none";
         active = false
     }
 })
